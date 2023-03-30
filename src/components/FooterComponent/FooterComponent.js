02a99fe0a2f55aeab24ae953d1986a32e236be83
angarcaz93@gmail.com <angarcaz93@gmail.com>
2023-03-30 17:29:47 +0200

    estilos ok

--- src/components/FooterComponent/FooterComponent.js
+++ src/components/FooterComponent/FooterComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin, FaSlideshare, FaYoutube } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 import "./footer.css"
 
@@ -8,19 +8,22 @@ const FooterComponent = () => (
   <div className='FooterComponentBox'>
      <footer className="Footer">
       <div className="FooterContainer">
-        <div className="SocialMedia">
-          <div className="Icons">
-            <a href="https://www.facebook.com/" target="_blank" rel="noreferrer"><FaFacebook /></a>
-            <a href="https://www.twitter.com/" target="_blank" rel="noreferrer"><FaTwitter /></a>
-            <a href="https://www.instagram.com/" target="_blank" rel="noreferrer"><FaInstagram /></a>
-          </div>
-        </div>
         <div className="ContactInfo">
-          <p>hello@Artbrowser.com</p>
+          <p>hello@artbrowser.com</p>
         </div>
         <div className="Legal">
           <p>© 2023 - Artbrowser</p>
         </div>
+        <div className="SocialMedia">
+          <div className="Icons">
+            <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
+            <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
+            <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+            <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+            <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer"><FaYoutube /></a>
+            <a href="https://www.slideshare.net/" target="_blank" rel="noopener noreferrer"><FaSlideshare /></a>
+          </div>
+      </div>
       </div>
     </footer>
   </div>
